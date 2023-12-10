0e427cd56d876a58ed20df21e9c0c80a4f44e393
meghakundu <54468245+meghakundu@users.noreply.github.com>
2023-12-10 08:38:05 +0530

    Update product.php

--- app/Models/product.php
+++ app/Models/product.php
@@ -5,8 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class product extends Model
 {
     use HasFactory;
-    protected $fillable=['title','price','description','prod_images','status','reject_reason'];
+    protected $fillable=['title','price','description','prod_images','status','reject_reason','user_id'];
+    
+    public function userProducts(){
+        return $this->belongsTo(Product::class,'user_id','id');
+    }
+
 }
